7080b498279161c3ed6088734af7bbc28945d8d2
upxrov <upyrov@outlook.com>
2026-08-02 01:01:09 +0300

    feat: invalidate lobby to rejoin list on leave

--- app/routes/lobbies/lobby.tsx
+++ app/routes/lobbies/lobby.tsx
@@ -45,6 +45,9 @@ export default function Lobby() {
     },
     onSuccess() {
       queryClient.invalidateQueries({ queryKey: queryKeys.lobbies.list({}) });
+      queryClient.invalidateQueries({
+        queryKey: queryKeys.lobbies.toRejoinList(),
+      });
       navigate("/");
     },
     onError(err) {
