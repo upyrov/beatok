0a37192beeeda9c7599ca8393a85a4a606f0bc48
upxrov <upyrov@outlook.com>
2026-08-01 13:40:09 +0300

    chore: add Netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), basicSsl()],
+  plugins: [tailwindcss(), reactRouter(), basicSsl(), netlifyPlugin()],
   resolve: {
     tsconfigPaths: true,
   },
